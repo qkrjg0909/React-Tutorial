6f407d07246c0aaf3298582403db0f251970bbcc
gypark <qkr0735@naver.com>
2019-06-05 17:31:28 +0900

    commit -work by comp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React , { Component} from 'react';
 //import logo from './logo.svg';
 import Customer from './components/Customer'
 import './App.css';
-
+//Hello WATO bro
 const customers =
   [
     {
